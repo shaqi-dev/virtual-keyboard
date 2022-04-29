041abaae4644fa7b4649e491bcc4582b433bffd4
Vladislav Zubakin <shaqimusic@gmail.com>
2022-04-30 01:46:59 +0300

    feat: extend codes

--- js/lib/codes.js
+++ js/lib/codes.js
@@ -39,4 +39,104 @@ export default {
             shift: '№'
         }
     },
+    'Digit4': {
+        en: {
+            default: '4', 
+            shift: '$'
+        },
+        ru: {
+            default: '4', 
+            shift: ';'
+        }
+    },
+    'Digit5': {
+        en: {
+            default: '5', 
+            shift: '%'
+        },
+        ru: {
+            default: '5', 
+            shift: '%'
+        }
+    },
+    'Digit6': {
+        en: {
+            default: '6', 
+            shift: '^'
+        },
+        ru: {
+            default: '6', 
+            shift: ':'
+        }
+    },
+    'Digit7': {
+        en: {
+            default: '7', 
+            shift: '&'
+        },
+        ru: {
+            default: '7', 
+            shift: '?'
+        }
+    },
+    'Digit8': {
+        en: {
+            default: '8', 
+            shift: '*'
+        },
+        ru: {
+            default: '8', 
+            shift: '*'
+        }
+    },
+    'Digit9': {
+        en: {
+            default: '9', 
+            shift: '('
+        },
+        ru: {
+            default: '9', 
+            shift: '('
+        }
+    },
+    'Digit0': {
+        en: {
+            default: '0', 
+            shift: ')'
+        },
+        ru: {
+            default: '0', 
+            shift: ')'
+        }
+    },
+    'NumpadSubtract': {
+        en: {
+            default: '-', 
+            shift: '_'
+        },
+        ru: {
+            default: '-', 
+            shift: '_'
+        }
+    },
+    'Equal': {
+        en: {
+            default: '=', 
+            shift: '+'
+        },
+        ru: {
+            default: '=', 
+            shift: '+'
+        }
+    },
+    'Backspace': {
+        en: {
+            default: '←Backspace', 
+            shift: '←Backspace'
+        },
+        ru: {
+            default: '←Backspace', 
+            shift: '←Backspace'
+        }
+    },
 }
