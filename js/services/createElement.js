b6200358ce3f743f29af4257ce83191b016f6c31
Vladislav Zubakin <shaqimusic@gmail.com>
2022-04-29 07:44:28 +0300

    feat: add class property to createElement service

--- js/services/createElement.js
+++ js/services/createElement.js
@@ -1,11 +1,13 @@
 export default function createElement({
-    type, 
+    type,
+    classes,
     styles, 
     attributes, 
     props, 
     eventHandlers, 
     appendTo }) {
     let elementType = type || 'div';
+    let elementClasses = classes || [];
     let elementStyles = styles || {};
     let elementAttributes = attributes || {};
     let elementProps = props || {};
@@ -13,6 +15,7 @@ export default function createElement({
     let elementAppendTo = appendTo || 'body';
   
     let element = document.createElement(elementType);
+    for (let className of elementClasses) { element.classList.add(className) }
     for (let key in elementStyles) { element.style[key] = elementStyles[key] }
     for (let key in elementAttributes) { element.setAttribute(key, elementAttributes[key]) }
     for (let key in elementProps) { element[key] = elementProps[key] }
