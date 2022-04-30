2c9859783d3d8d3debe2ad5771aaf88f28fa5541
Vladislav Zubakin <shaqimusic@gmail.com>
2022-04-30 03:09:12 +0300

    feat: add keyevents

--- js/index.js
+++ js/index.js
@@ -25,10 +25,10 @@ createElement({
     appendTo: '.wrapper'
 });
 
+const textarea = document.querySelector('#textarea');
+
 function hanleKeyClick(e) {
-    const textarea = document.querySelector('#textarea');
     const keyText = e.target.innerText;
-    
     textarea.value += keyText;
 }
 
@@ -44,9 +44,80 @@ for (let key in codes) {
     });
 }
 
-const keysDefaultEN = document.querySelectorAll('.key__values_en .key__value_default');
-const keysOnShiftEN = document.querySelectorAll('.key__values_en .key__value_shift');
-const keysDefaultRU = document.querySelectorAll('.key__values_ru .key__value_default');
-const keysOnShiftRU = document.querySelectorAll('.key__values_ru .key__value_shift');
+const languages = {
+    en: false,
+    ru: true
+}
 
-keysDefaultEN.forEach(key => key.classList.add('key__value_active'));
+const keys = document.querySelectorAll('.key');
+const keyValues = document.querySelectorAll('.key__value');
+const keyValuesDefaultEN = document.querySelectorAll('.key__values_en .key__value_default');
+const keyValuesShiftEN = document.querySelectorAll('.key__values_en .key__value_shift');
+const keyValuesDefaultRU = document.querySelectorAll('.key__values_ru .key__value_default');
+const keysValuesShiftRU = document.querySelectorAll('.key__values_ru .key__value_shift');
+
+console.log();
+
+function removeActiveClasses() {
+    keyValues.forEach(key => key.classList.remove('key__value_active'));
+}
+
+function setActiveDefaultKeys() {
+    removeActiveClasses();
+    const keyValues = languages.en ? keyValuesDefaultEN : keyValuesDefaultRU
+    keyValues.forEach(key => key.classList.add('key__value_active'));
+}
+
+setActiveDefaultKeys();
+
+function toggleActiveLanguage() {
+    languages.en = !languages.en
+    languages.ru = !languages.ru
+    setActiveDefaultKeys();
+}
+
+const keyMap = {};
+
+function setActiveShiftKeys() {
+    removeActiveClasses();
+    const keyValues = languages.en ? keyValuesShiftEN : keysValuesShiftRU
+    keyValues.forEach(key => key.classList.add('key__value_active'));
+}
+
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keypress', handleKeyPress);
+document.addEventListener('keyup', handleKeyUp);
+
+function handleKeyDown(e) {
+    const keyOnKeyboard = [...keys].find(key => key.dataset.code === e.code);
+
+    if (keyOnKeyboard) { keyOnKeyboard.classList.add('key_active'); }
+
+    if (!keyMap[e.code] || keyMap[e.code] === false) {
+        keyMap[e.code] = true; 
+        if (keyMap['ShiftLeft'] && keyMap['AltLeft']) {
+            toggleActiveLanguage();
+        };
+        if (keyMap['ShiftLeft'] && !keyMap['AltLeft']) {
+            setActiveShiftKeys();
+        };
+
+        console.log(keyMap);
+    }
+}
+
+function handleKeyPress(e) {
+    textarea.value += e.key;
+}
+
+function handleKeyUp(e) {
+    const keyOnKeyboard = [...keys].find(key => key.dataset.code === e.code);
+
+    if (keyOnKeyboard) { keyOnKeyboard.classList.remove('key_active'); }
+
+    keyMap[e.code] = false;
+    
+    setActiveDefaultKeys();
+}
+
+
